db0a027fea1baba6e8bfc85f078f8557684876b6
Bruno Souza <ext.bruno.souza@farfetch.com>
2024-02-07 06:33:39 -0300

    Add loading state

--- composeApp/src/commonMain/kotlin/br/com/souzabrunoj/storekmp/di/Di.kt
+++ composeApp/src/commonMain/kotlin/br/com/souzabrunoj/storekmp/di/Di.kt
@@ -2,7 +2,7 @@ package br.com.souzabrunoj.storekmp.di
 
 import br.com.souzabrunoj.storekmp.data.HomeRepositoryImpl
 import br.com.souzabrunoj.storekmp.domain.repository.HomeRepository
-import br.com.souzabrunoj.storekmp.presentation.HomeViewModel
+import br.com.souzabrunoj.storekmp.presentation.screenModel.HomeViewModel
 import org.koin.dsl.module
 
 val commonModule = module {
